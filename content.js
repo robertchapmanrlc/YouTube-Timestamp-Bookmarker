af9b6540c54624b8183912e30e930b135ad3da09
Robert Chapman <chapmanrobert12@gmail.com>
2024-01-06 13:31:13 -0600

    The bookmarked timestamps are sent from the web page to the popup to be displayed in a list

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessag.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action == "bookmark") {
     let videoId = window.location.search.split("v=")[1];
     let timestamp = request.timestamp;
@@ -7,7 +7,13 @@ chrome.runtime.onMessag.addListener((request, sender, sendResponse) => {
       let bookmarks = JSON.parse(localStorage.getItem(videoId)) || [];
       bookmarks.push(timestamp);
       localStorage.setItem(videoId, JSON.stringify(bookmarks));
+      sendResponse({ bookmarks: bookmarks });
       alert("Timestamp bookmarked successfully!");
     }
+  } else if (request.action == 'getBookmarks') {
+    let videoId = window.location.search.split("v=")[1];
+    let bookmarks = JSON.parse(localStorage.getItem(videoId));
+    console.log(bookmarks);
+    sendResponse({ bookmarks: bookmarks });
   }
 });
--- popup.js
+++ popup.js
@@ -1,11 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   let timeStampInput = document.getElementById("timestampInput");
   let bookmarkButton = document.getElementById("bookmarkBtn");
+  let bookmarkList = document.getElementById('bookmarkList');
 
   bookmarkButton.addEventListener("click", () => {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       let activeTab = tabs[0];
-      chrome.tabs.sendMessage(activeTab.id, { action: 'bookmark', timestamp: timeStampInput.value });
+      chrome.tabs.sendMessage(activeTab.id, { action: 'bookmark', timestamp: timeStampInput.value }, (response) => {
+        loadBookmarks(response.bookmarks);
+      });
+    });
+  });
+
+  function loadBookmarks(bookmarks) {
+    bookmarkList.innerHTML = '';
+
+    bookmarks.forEach((timestamp) => {
+      let listItem = document.createElement('li');
+      listItem.textContent = timestamp;
+      bookmarkList.appendChild(listItem);
+    });
+  }
+
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { action: 'getBookmarks' }, (response) => {
+      loadBookmarks(response.bookmarks);
     });
   });
 });
