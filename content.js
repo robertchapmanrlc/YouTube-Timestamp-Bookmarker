41d06387eaf42ac2c1b28ca21fef8058e061fb1a
Robert Chapman <chapmanrobert12@gmail.com>
2024-01-06 23:22:52 -0600

    Refined the process to isolate videoId from url

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action == "bookmark") {
-    let videoId = window.location.search.split("v=")[1];
+    let videoId = getVideoIdFromUrl(window.location.href);
     let timestamp = request.timestamp;
 
     if (videoId && timestamp) {
@@ -11,9 +11,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       alert("Timestamp bookmarked successfully!");
     }
   } else if (request.action == 'getBookmarks') {
-    let videoId = window.location.search.split("v=")[1];
+    let videoId = getVideoIdFromUrl(window.location.href);
     let bookmarks = JSON.parse(localStorage.getItem(videoId));
     console.log(bookmarks);
     sendResponse({ bookmarks: bookmarks });
   }
 });
+
+function getVideoIdFromUrl(url) {
+  let urlParts = url.split('&');
+
+  for (let i = 0; i < urlParts.length; i += 1) {
+    if (urlParts[i].startsWith('v=')) {
+      return urlParts[i].substring(2);
+    }
+  }
+
+  let videoIdMatches = url.match(/[?&]v=([^&#]+)/);
+  return videoIdMatches ? videoIdMatches[1] : null;
+}
